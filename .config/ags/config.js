7638672f199af3a1e272223041a8ac1b2bc06621
[Saiiru] <[sairufsa@gmail.com]>
2024-11-13 09:33:38 -0300

    refactor: refactoring my overaal config

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,88 +1,60 @@
-import { Utils, App, Notifications } from './modules/utils/imports.js';
-import { Bar } from './modules/topbar.js';
-import { MainMenu } from './modules/menus/MainMenu.js';
-import { VolumeOSD } from './modules/on-screen/volume.js';
-import MyNotifications from './modules/notifications/OSDNotifications.js';
-import { PrayerTimesMenu } from './modules/menus/PrayerTimesMenu.js';
-import ColorWidget from './modules/widgets/desktop/ColorsWidget.js';
-import win20Widget from './modules/widgets/desktop/Win20Widget.js';
-import materialWidget from './modules/widgets/desktop/MaterialYouOne.js';
-import unicatWidget from './modules/widgets/desktop/UnicatWidget.js';
-import blackHoleWidget from './modules/widgets/desktop/BlackHole.js';
-import goldenWidget from './modules/widgets/desktop/Golden.js';
-import harmonyWidget from './modules/widgets/desktop/Harmony.js';
-import newCatWidget from './modules/widgets/desktop/NewCat.js';
-import deerWidget from './modules/widgets/desktop/DeerWidget.js';
-import circlesMusicWidget from './modules/widgets/desktop/Circles.js';
-import whiteFlowerWidget from './modules/widgets/desktop/WhiteFlower.js';
-import { CalendarMenu } from './modules/menus/CalendarMenu.js';
-import settings from './modules/settings.js';
-import { applauncher } from './modules/menus/ApplicationsMenu.js';
+import GLib from "gi://GLib";
 
-import {
-    TopLeftCorner,
-    TopRightCorner,
-} from './modules/components/ScreenCorners.js';
-import { languageLayoutOSD } from './modules/on-screen/KeyboardLayout.js';
+const main = "/tmp/asztal/main.js";
+const entry = `${App.configDir}/main.ts`;
+const bundler = GLib.getenv("AGS_BUNDLER") || "bun";
 
-// in config.js
-const scss = App.configDir + '/scss/main.scss';
-const css = App.configDir + '/style.css';
+const v = {
+  ags: pkg.version?.split(".").map(Number) || [],
+  expect: [1, 8, 1],
+};
 
-Utils.exec(`sassc ${scss} ${css}`);
+try {
+  switch (bundler) {
+    case "bun":
+      await Utils.execAsync([
+        "bun",
+        "build",
+        entry,
+        "--outfile",
+        main,
+        "--external",
+        "resource://*",
+        "--external",
+        "gi://*",
+        "--external",
+        "file://*",
+      ]);
+      break;
 
-let windows = [
-    VolumeOSD(),
-    MyNotifications(),
-    PrayerTimesMenu(),
-    CalendarMenu(),
-    languageLayoutOSD,
-    // ... Desktop widgets ... //
-    ColorWidget,
-    win20Widget,
-    materialWidget,
-    unicatWidget,
-    blackHoleWidget,
-    goldenWidget,
-    harmonyWidget,
-    newCatWidget,
-    deerWidget,
-    circlesMusicWidget,
-    whiteFlowerWidget,
-    applauncher,
-];
+    case "esbuild":
+      await Utils.execAsync([
+        "esbuild",
+        "--bundle",
+        entry,
+        "--format=esm",
+        `--outfile=${main}`,
+        "--external:resource://*",
+        "--external:gi://*",
+        "--external:file://*",
+      ]);
+      break;
 
-const screens = JSON.parse(Utils.exec('hyprctl monitors all -j'));
+    default:
+      throw `"${bundler}" is not a valid bundler`;
+  }
 
-for (let i = 0; i < screens.length; i++) {
-    const screen = screens[i];
+  if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
+    print(
+      `my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`,
+    );
+    App.quit();
+  }
 
-    windows.push(Bar({ monitor: screen.id }));
-
-    const leftMene = MainMenu({ monitor: screen.id });
-    windows.push(leftMene);
-    windows.push(TopLeftCorner({ monitor: screen.id }));
-    windows.push(TopRightCorner({ monitor: screen.id }));
+  await import(`file://${main}`);
+} catch (error) {
+  console.error(error);
+  App.quit();
 }
 
-Notifications.cacheActions;
-globalThis.getNot = () => Notifications;
-
-Utils.execAsync([`paplay`, settings.assets.audio.desktop_login]).catch(print);
-
-Utils.execAsync([
-    'python',
-    settings.scripts.createThumbnail,
-    settings.theme.darkM3WallpaperPath,
-]).catch(print);
-
-Utils.execAsync([
-    'python',
-    settings.scripts.createThumbnail,
-    settings.theme.lightM3WallpaperPath,
-]).catch(print);
-
-App.config({
-    css: css,
-    windows: windows,
-});
+export {};
