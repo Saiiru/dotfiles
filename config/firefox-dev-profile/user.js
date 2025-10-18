fff861db57329448cb95bebb40d841a5c8c7f5b0
Saiiru <sairufsa@gmail.com>
2025-10-18 19:42:49 -0300

    feat(dotfiles): Re-applied cleanup, installation scripts, and Neovim config

---
+++ config/firefox-dev-profile/user.js
@@ -0,0 +1,7 @@
+// ===== VA-API / AMD =====
+user_pref("media.ffmpeg.vaapi.enabled", true);
+
+// ===== Portals (file picker, MIME, etc) =====
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
+user_pref("widget.use-xdg-desktop-portal.settings", 1);
